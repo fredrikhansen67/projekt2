bcfd96298c714219710d397e281e572d84d39687
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-14 13:17:36 +0100

    draft

--- Airline/src/AirlineBookingController.java
+++ Airline/src/AirlineBookingController.java
@@ -12,5 +12,26 @@ public class AirlineBookingController {
 	// Customer c = new Customer("","","","","");
 	//Boing737 flyg1 = new Boing737();
 	//flyg.addBooking(c, seatPosition);
+	
+	public void initSystem(){
+		//Skapa alla flygplan.
+		//Boing737 flyg1 = new Boing737();
+		//Boing737 flyg2 = new Boing737();
+		//Boing737 flyg3 = new Boing737();
+		//Boing737 flyg4 = new Boing737();
+		//Boing737 flyg5 = new Boing737();
+		//aircraftsList.add(flyg1);
+		//aircraftsList.add(flyg2);
+		//aircraftsList.add(flyg3);
+		//aircraftsList.add(flyg4);
+		//aircraftsList.add(flyg5);
+		//Cutomer cus1 = new Customer("Kalle",22,33,44);
+		//Cutomer cus2 = new Customer();
+		//Cutomer cus3 = new Customer();
+	}
+	
+	public void addBooking(){
+		
+	}
 
 }
