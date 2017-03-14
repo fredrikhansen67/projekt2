a41417499244ae04ffe56b0fe549accc9e326a59
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-14 13:09:53 +0100

    booking first draft

---
+++ Airline/src/AirlineBookingController.java
@@ -0,0 +1,16 @@
+
+
+public class AirlineBookingController {
+	//TODO Skapa flygplan, skapa Customer, hantera uppdateringar.....
+	
+	
+	// public static ArrayList<AirlineBookingController> bookingList = new ArrayList();
+	// public static ArrayList<Aircraft> aircraftsList = new ArrayList<Aircraft>();
+	// public static ArrayList<Customer> customerList = new ArrayLsit();
+	
+	
+	// Customer c = new Customer("","","","","");
+	//Boing737 flyg1 = new Boing737();
+	//flyg.addBooking(c, seatPosition);
+
+}
