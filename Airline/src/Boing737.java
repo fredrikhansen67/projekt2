4f022edf3966246c010c04df8a4f867fb3657cf0
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-14 14:04:51 +0100

    fresh

---
+++ Airline/src/Boing737.java
@@ -0,0 +1,20 @@
+
+
+public class Boing737 extends Aircraft {
+	private String name ;
+
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
