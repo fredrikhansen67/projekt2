cf7a8db49e2517d6fc44a23c5bf975b1f572db27
emilvol-Java <User@172.16.1.68>
2017-03-14 13:24:06 +0100

    Customer och UI.

---
+++ Airline/src/Customer.java
@@ -0,0 +1,51 @@
+
+
+public class Customer {
+
+	private static int customerID;                // Final på den här ?? 
+	private String name;
+	private int age;
+	private int telephone;
+	private int personalNumber;
+	
+	
+	public Customer(String name, int age, int telephone, int personalNumber) {
+		
+		this.name = name;
+		this.age = age;
+		this.telephone = telephone;
+		this.personalNumber = personalNumber;
+		customerID++; 
+	
+	}
+
+
+	public static int getCustomerID() {
+		return customerID;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public int getTelephone() {
+		return telephone;
+	}
+
+
+	public int getPersonalNumber() {
+		return personalNumber;
+	}
+	
+	
+	
+	
+	
+}
---
+++ Airline/src/UI.java
@@ -0,0 +1,37 @@
+
+
+import java.util.ArrayList;
+
+public class UI {
+	
+	private Customer customer;
+	private double TotalCompanyIncome;
+	
+	public void printCustomerCosts(Customer customer){
+		// Prints costs for each customer (food + ticket)
+	}
+	
+//	public void printTotalCompanyIncome(ArrayList<Booking> bookings){
+//		// Prints costs for each customer (food + ticket)
+//		
+//		for (Booking booking : bookings ){
+//			
+//			//... calculate and print
+//		}
+//		
+//	}
+	
+	public void printCompanyProfit(){
+		// 0.3 * TotalCompanyIncome
+		
+	}
+	
+	public void printCustomerDetails(Customer customer){
+		
+		System.out.println("Customer name: " + customer.getName()
+						+ "\nCustomerID: " + customer.getCustomerID());
+	
+	}
+	
+	
+}
