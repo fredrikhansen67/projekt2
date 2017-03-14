5da84d6447fefb2ada7b0029e095a7a07f960ffc
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-14 14:06:47 +0100

    dd

--- Airline/src/Airline.java
+++ Airline/src/Airline.java
@@ -3,7 +3,9 @@ public class Airline {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		AirlineBookingController ac = new AirlineBookingController();
+		ac.initSystem();
+		System.out.println(ac.toString());
 	}
 
 }
