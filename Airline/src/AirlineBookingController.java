3dc1420baf6e07aecfbb8e407584bf355b0955ea
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-15 11:03:16 +0100

    update constructor

--- Airline/src/AirlineBookingController.java
+++ Airline/src/AirlineBookingController.java
@@ -28,14 +28,14 @@ public class AirlineBookingController {
 		aircraftsList.add(flyg3);
 		aircraftsList.add(flyg4);
 		aircraftsList.add(flyg5);
-		Customer cus1 = new Customer("Kalle",22,33,44);
-		Customer cus2 = new Customer("frasse",22,33,44);
-		Customer cus3 = new Customer("pelle",22,33,44);
+		Customer cus1 = new Customer("Kalle",22,"33","44");
+		Customer cus2 = new Customer("frasse",22,"33","44");
+		Customer cus3 = new Customer("pelle",22,"33","44");
 	}
 	
-	public void addBooking(String name ,int age, int phone , int socialNumber, Aircraft flight, int seat){
+	public void addBooking(String name ,int age, String phone , String socialNumber, String flight, String cabinclass, int seat){
 		Customer cus = new Customer( name , age,  phone ,  socialNumber);
-		Booking book = new Booking( cus, flight.getName(), seat);
+		Booking book = new Booking( cus, flight, seat);
 		
 		for(Aircraft ac:aircraftsList){
 			//TODO leta upp flighten och stoppa in i flyglistan på position 'seat'
