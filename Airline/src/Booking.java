393dceb596c98ad6ba0fa69d0f743db3613ccbc3
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-15 11:16:38 +0100

    update methods and constructor

--- Airline/src/Booking.java
+++ Airline/src/Booking.java
@@ -4,12 +4,31 @@ public class Booking {
 	
 	private Customer cus;
 	private String ac;
+	private String cabinclass;
 	private int seatNumber;
 	
-	public Booking(Customer cus, String ac, int seatNumber){
+	public Booking(Customer cus, String ac, String cabinclass, int seatNumber){
 		this.cus = cus;
 		this.ac = ac;
+		this.cabinclass = cabinclass;
 		this.seatNumber = seatNumber;
 	}
+
+	public Customer getCus() {
+		return cus;
+	}
+
+	public String getAc() {
+		return ac;
+	}
+
+	public String getCabinclass() {
+		return cabinclass;
+	}
+
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+
 
 }
