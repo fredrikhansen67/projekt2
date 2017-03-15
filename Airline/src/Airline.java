79d4a44775f9e81c16658b947c9f4f02e596de0e
emilvol-Java <User@172.16.1.68>
2017-03-15 09:06:07 +0100

    kjk

--- Airline/src/Airline.java
+++ Airline/src/Airline.java
@@ -4,6 +4,21 @@ public class Airline {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AirlineBookingController ac = new AirlineBookingController();
+
+		ac.initSystem();
+		
+		
+		System.out.println(ac);
+		
+
+	       javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	            	AirlineGui aGui = new AirlineGui();
+	            }
+	        });
+		
+		
+
 	
 			ac.initSystem();
 			AirlineGui gui = new AirlineGui();
@@ -11,5 +26,10 @@ public class Airline {
 			System.out.println(ac.toString());
 		
 	}
+	
+	
+
+
+	
 
 }
