68d70e50c01dc998e2d9c037f5ccf44af026ad0c
fredrikhansen67 <fredrikhansen67@172.16.1.137>
2017-03-17 16:38:03 +0100

    dsd

--- Airline/src/PricingInterface.java
+++ Airline/src/PricingInterface.java
@@ -2,7 +2,7 @@
 public interface PricingInterface {
 	
 	public static final int FIRST_CLASS_PRICE = 20000;
-	public static final int ECONOMY_CLASS_PRICE = 2000;
+	public static final int ECONOMY_CLASS_PRICE = 5000;
 	public static final int FIRST_CLASS_MEAL = 250;
 	public static final int ECONOMY_CLASS_MEAL = 100;
 
